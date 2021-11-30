5c7636433d156e14e1adc8cfcc8be17cd93a891d
zivm20 <ziv.morgan@gmail.com>
2021-11-30 21:33:48 +0200

    added main program

--- main.c
+++ main.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
 #include "my_mat.h"
 int main(){
-  
+
   int arr[N*N];
-  for(int i=0; i<N*N;i++){
-    scanf("%d",&arr[i]);
-  }
   int mat[N][N];
+  char functionType;
+  int first_print = true;
+  while(functionType != "D"){
+    scanf("%c",&functionType);
+    if(functionType == "A"){
+      for(int i=0; i<N*N;i++){
+        scanf("%d",&arr[i]);
+      }
+      function_A(arr,mat);
+    }
 
-  function_A(arr,mat);
-  printArr(mat);
+    else if(functionType == "B" || functionType=="C"){
+      if(firstPrint == true){
+        firstPrint = false;
+      }
+      else{
+        printf("\n");
+      }
+      int src;
+      int dest;
+      scanf("%d",&src);
+      scanf("%d",&dest);
+      if(functionType == "B"){
+        function_B(mat[N][N]);
+      }
+      else{
+        printf("%d",function_C);
+      }
+    }
 
+  }
+  //printf("\n\n\n");
+  //printArr(mat);
+  //printf("\n\n\n");
+
+  //scanf("%d",&src);
+  //scanf("%d",&dest);
 
   return 0;
 }
